5c13ae225eabb509ba1d67871a42028a51d343e9
jaywcjlove <398188662@qq.com>
2018-03-19 15:22:08 +0800

    Add uiw.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button } from 'uiw';
 import logo from './logo.svg';
 import './App.css';
 
@@ -13,6 +14,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button type="primary">主要按钮</Button>
+        <Button type="success">成功按钮</Button>
+        <Button type="info">信息按钮</Button>
+        <Button type="warn">警告按钮</Button>
+        <Button type="danger">错误按钮</Button>
       </div>
     );
   }
