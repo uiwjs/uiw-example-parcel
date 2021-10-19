b6469782322b07edcb46c019d7e94be94c93694b
jaywcjlove <398188662@qq.com>
2021-10-19 10:23:13 +0800

    chore(deps): update dependency parcel to v2.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Button } from 'uiw';
-import logo from './logo.svg';
+import Logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button type="primary">主要按钮</Button>
-        <Button type="success">成功按钮</Button>
-        <Button type="warning">警告按钮</Button>
-        <Button type="danger">错误按钮</Button>
-        <Button type="light">亮按钮</Button>
-        <Button type="dark">暗按钮</Button>
-      </div>
-    );
-  }
-}
-
-export default App;
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Logo className="App-logo" height="60" width="60" viewBox="0 0 20 20" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+      <Button type="primary">主要按钮</Button>
+      <Button type="success">成功按钮</Button>
+      <Button type="warning">警告按钮</Button>
+      <Button type="danger">错误按钮</Button>
+      <Button type="light">亮按钮</Button>
+      <Button type="dark">暗按钮</Button>
+    </div>
+  );
+};
